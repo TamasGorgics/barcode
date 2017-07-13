bf85d2f468f12e0e8f21c8914df06838930b17b7
Tamás Görgics <toma2067@gmail.com>
2017-07-13 17:53:44 +0200

    fix image path assembler

--- src/main/java/com/tgorgics/barcode/util/BarcodeUtil.java
+++ src/main/java/com/tgorgics/barcode/util/BarcodeUtil.java
@@ -7,10 +7,11 @@ public class BarcodeUtil {
     public static String assembleImagePath(final String servletPath, final BarcodeDTO barcode) {
 
         final StringBuilder sb = new StringBuilder(servletPath).append("?data=").append(barcode.getData());
-        sb.append(barcode.getLabel() == null ? "" : "&label=" + barcode.getLabel());
+        sb.append(barcode.getLabel() == null || barcode.getLabel() == "" ? "" : "&label=" + barcode.getLabel());
         sb.append(barcode.isDrawText() == false ? "" : "&drawText=true");
         sb.append(barcode.getWidth() == 0 ? "" : "&width=" + barcode.getWidth());
         sb.append(barcode.getHeight() == 0 ? "" : "&height=" + barcode.getHeight());
+        sb.append("&headless=false&resolution=-1");
 
         return sb.toString();
     }
